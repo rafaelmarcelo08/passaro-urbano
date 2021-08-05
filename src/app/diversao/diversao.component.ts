08bbf4662bab6f1dee9ef323eb64814e85fd97fc
Rafael <rafael.2018.ads@gmail.com>
2021-08-05 16:06:57 -0300

    Criando as paginas Restaurantes e Diversao

---
+++ src/app/diversao/diversao.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diversao',
+  templateUrl: './diversao.component.html',
+  styleUrls: [
+    './diversao.component.css'
+  ]
+})
+export class DiversaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
