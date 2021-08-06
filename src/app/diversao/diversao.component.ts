7385e476f63067bcd7c9d80c8aa798c96bc4e158
Rafael <rafael.2018.ads@gmail.com>
2021-08-06 11:26:38 -0300

    Recuperando ofertas do tipo Diversao

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,17 +1,31 @@
+import { OfertaModel } from './../shared/oferta.model';
 import { Component, OnInit } from '@angular/core';
+
+import { OfertasService } from './../services/ofertas.service';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
   styleUrls: [
     './diversao.component.css'
+  ],
+  providers: [
+    OfertasService
   ]
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+  public ofertas !: OfertaModel[];
+
+  constructor(
+    private ofertasService: OfertasService
+  ) { }
 
   ngOnInit(): void {
+    this.ofertasService.getOfertasPorCategoria('diversao')
+      .then((ofertas: OfertaModel[]) => {
+        this.ofertas = ofertas;
+      });
   }
 
 }
