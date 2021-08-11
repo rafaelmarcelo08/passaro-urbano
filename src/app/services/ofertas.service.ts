78ce7187c348166131810261b71abdeec0a3dc7a
Rafael <rafael.2018.ads@gmail.com>
2021-08-11 10:49:12 -0300

    Ajustando a url de acesso a API

--- src/app/services/ofertas.service.ts
+++ src/app/services/ofertas.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { OfertaModel } from "../shared/oferta.model";
+import { URL_API } from "../app.api";
 
 @Injectable()
 export class OfertasService {
@@ -11,19 +12,19 @@ export class OfertasService {
     ) { }
 
     public getOfertas(): Promise<OfertaModel[]> {
-        return this.http.get<OfertaModel[]>('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get<OfertaModel[]>(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta);
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<OfertaModel[]> {
-        return this.http.get<OfertaModel[]>(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get<OfertaModel[]>(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta) => resposta);
     }
 
     public getOfertaPorId(id: Number): Promise<OfertaModel> {
-         return this.http.get<OfertaModel>(`http://localhost:3000/ofertas?id=${id}`)
+         return this.http.get<OfertaModel>(`${URL_API}?id=${id}`)
             .toPromise()
             .then((resposta: any) =>  {
                 return resposta.shift()
