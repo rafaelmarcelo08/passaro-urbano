581b0571c10e40725436c1640b36d0d7a3122a8c
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-08-30 16:08:02 -0300

    Passando parametro para um pipe customizado

--- src/app/util/descricao-reduzia.pipe.ts
+++ src/app/util/descricao-reduzia.pipe.ts
@@ -5,10 +5,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class DescricaoReduzida implements PipeTransform {
 
-    transform(value: string): string {
+    transform(value: string,truncarEm: number): string {
 
-        if (value.length > 15) {
-            return value.substr(0, 15) + '...';
+        if (value.length > truncarEm) {
+            return value.substring(0, truncarEm) + '...';
         }
 
         return value;
