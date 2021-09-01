1147c01e3c2d6beb620908c83617b8911b508be2
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-09-01 16:48:06 -0300

    Two-Way-Binding dos dados do formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: String = '';
+  public numero: String = '';
+  public complemento: String = '';
+  public formaPagamento: String = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public atualizaEndereco(endereco: String): void {
+    this.endereco = endereco;
+    console.log(this.endereco);
+  }
+
+  public atualizaNumero(numero: string): void {
+    this.numero = numero;
+    console.log(this.numero);
+  }
+
+  public atualizaComplemento(complemento: String): void {
+    this.complemento = complemento;
+    console.log(this.complemento);
+  }
+
+  public atualizaFormaPagamento(formaPagamento: String): void {
+    this.formaPagamento = formaPagamento;
+    console.log(this.formaPagamento);
+  }
+
 }
+
