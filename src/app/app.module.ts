7848ea9fdb2f3e8e666f53bff2d79c852e423f54
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-09-08 15:00:05 -0300

    Importado modulo FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
+/**Serviços do angular */
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import {
   LOCALE_ID,
   NgModule
 } from '@angular/core';
 
+/**Componentes */
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
-import { ROUTES } from './app.routes';
 import { OfertaComponent } from './oferta/oferta.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-import { DescricaoReduzida } from './util/descricao-reduzia.pipe';
 import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component';
+
+/**Rotas */
+import { ROUTES } from './app.routes';
+
+/**Pipes */
+import { DescricaoReduzida } from './util/descricao-reduzia.pipe';
 
 registerLocaleData(localePt);
 
@@ -42,7 +49,8 @@ registerLocaleData(localePt);
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [
     {
