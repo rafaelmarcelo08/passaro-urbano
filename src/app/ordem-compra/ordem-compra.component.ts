e2988adb77aae040e89274815c96853bbbf843b8
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-09-13 12:24:34 -0300

    Criando um FormGroup na classe do componente

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
+
 import { OrdemCompraService } from '../services/ordem-compra.service';
 import { Pedido } from '../model/pedido.model';
 
@@ -6,18 +14,30 @@ import { Pedido } from '../model/pedido.model';
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ 
+  providers: [
     OrdemCompraService
   ]
 })
 export class OrdemCompraComponent implements OnInit {
 
-  constructor(private ordemCompraService: OrdemCompraService) { }
+  constructor(
+    private ordemCompraService: OrdemCompraService
+  ) { }
+
+  public formulario: FormGroup = new FormGroup(
+    {
+      'endereco': new FormControl(null),
+      'numero': new FormControl(null),
+      'complemento': new FormControl(null),
+      'formaPagamento': new FormControl(null)
+    }
+  );
 
   ngOnInit() {
-    
+
   }
 
   public confirmarCompra(): void {
+    console.log(this.formulario.value);
   }
 }
